041ffaa3adc396c9ca3bf2c223181aaf694bf4a4
lucasmenezesroehrs <162138110+lucasmenezesroehrs@users.noreply.github.com>
2024-10-21 11:42:39 -0300

    ernwui

---
+++ perguntas.js
@@ -0,0 +1,42 @@
+criarCartao{
+    'Programaçao'
+    'o que é o CSS?'
+    'O CSS é uma linguagem de estilização'
+}
+criarCartao{
+    'Programaçao'
+    'Para que serve o CSS?'
+    'CSS (Folhas de Estilo em Cascata) permite a você criar páginas web agradáveis.'
+}
+criarCartao{
+    'Programaçao'
+    'Como funciona o Css?'
+    'O CSS é uma linguagem de estilo utilizada na construção de páginas para definir a apresentação visual de documentos HTML. Enquanto o HTML é responsável por estruturar o conteúdo da página web, o CSS determina como ele será exibido. Isso inclui aspectos como cores, fontes, espaçamentos, alinhamentos, tamanhos e layouts.'
+}
+criarCartao{
+    ''
+    'o que é o CSS?'
+    'O CSS é uma linguagem de estilização'
+}
+criarCartao{
+    'Programaçao'
+    'o que é o CSS?'
+    'O CSS é uma linguagem de estilização'
+}
+criarCartao{
+    'Programaçao'
+    'o que é o CSS?'
+    'O CSS é uma linguagem de estilização'
+}
+criarCartao{
+    'Programaçao'
+    'o que é o CSS?'
+    'O CSS é uma linguagem de estilização'
+}
+criarCartao{
+    'Programação'
+    
+}
+
+
+    
