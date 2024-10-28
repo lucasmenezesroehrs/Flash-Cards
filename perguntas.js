d36754c22d11055a6b28a0e291d83b1f6163ab32
lucasmenezesroehrs <162138110+lucasmenezesroehrs@users.noreply.github.com>
2024-10-28 11:43:51 -0300

    ,,

--- perguntas.js
+++ perguntas.js
@@ -1,47 +1,47 @@
-criarCartao{
-    'Programaçao'
-    'o que é o CSS?'
+criarCartao(
+    'Programaçao',
+    'o que é o CSS?',
     'O CSS é uma linguagem de estilização'
-}
-criarCartao{
-    'Programaçao'
-    'Para que serve o CSS?'
+)
+criarCartao(
+    'Programaçao',
+    'Para que serve o CSS?',
     'CSS (Folhas de Estilo em Cascata) permite a você criar páginas web agradáveis.'
-}
-criarCartao{
-    'Programaçao'
-    'Como funciona o Css?'
+)
+criarCartao(
+    'Programaçao',
+    'Como funciona o Css?',
     'O CSS é uma linguagem de estilo utilizada na construção de páginas para definir a apresentação visual de documentos HTML. Enquanto o HTML é responsável por estruturar o conteúdo da página web, o CSS determina como ele será exibido. Isso inclui aspectos como cores, fontes, espaçamentos, alinhamentos, tamanhos e layouts.'
-}
-criarCartao{
-    'historia'
-    'Quais foram as principais consequências sociais e políticas da ditadura militar no Brasil, e como elas ainda influenciam a sociedade brasileira contemporânea?'?'
-    'Em seu primeiro governo a ditadura pareceu tolerar ou negligenciar a cultura de protesto (música, cinema, literatura, artes plásticas) elaborada por artistas e intelectuais que, através de sua arte e de seu humor, criticavam a censura e o regime, incentivavam a rebeldia e denunciavam o terrorismo cultural'
-}
-criarCartao{
-    'historia'
-    'No que tange à Segunda Guerra Mundial, marque a opção INCORRETA:'
-    'A cidade que os nazistas promoveram um cerco para deixar a população morrer de fome foi a cidade de Stalingrado, nas margens do rio Volga.'
-}
-criarCartao{
-    'historia'
-    'Ao final da Primeira Guerra Mundial, instalou-se na Alemanha uma Assembleia Nacional que se reuniu em Weimar para preparar uma nova Constituição para o país. Nascia ali um novo Estado, chamado de:'
+)
+criarCartao(
+    'histpria',
+    'Ao final da Primeira Guerra Mundial, instalou-se na Alemanha uma Assembleia Nacional que se reuniu em Weimar para preparar uma nova Constituição para o país. Nascia ali um novo Estado, chamado de:',
     'República de Weimar.'
-}
-criarCartao{
-    'Sociologia'
-    'Antropologia é a designação comum a diferentes ciências cuja finalidade é descrever o ser humano e analisá-lo com base nas características biológicas e socioculturais dos diversos grupos em que se distribui, dando ênfase às diferenças e variações entre esses grupos. Uma destas ciências, a Antropologia Biológica, que tem por objeto a variação biológica do ser humano, tanto em seu desenvolvimento evolutivo quanto em sua expressão histórica e contemporânea, é também conhecida como Antropologia'
+)
+criarCartao(
+    'historia',
+    'No que tange à Segunda Guerra Mundial, marque a opção INCORRETA:',
+    'A cidade que os nazistas promoveram um cerco para deixar a população morrer de fome foi a cidade de Stalingrado, nas margens do rio Volga.'
+)
+criarCartao(
+    'historia',
+    'Ao final da Primeira Guerra Mundial, instalou-se na Alemanha uma Assembleia Nacional que se reuniu em Weimar para preparar uma nova Constituição para o país. Nascia ali um novo Estado, chamado de:',
+    'República de Weimar.'
+)
+criarCartao(
+    'Sociologia',
+    'Antropologia é a designação comum a diferentes ciências cuja finalidade é descrever o ser humano e analisá-lo com base nas características biológicas e socioculturais dos diversos grupos em que se distribui, dando ênfase às diferenças e variações entre esses grupos. Uma destas ciências, a Antropologia Biológica, que tem por objeto a variação biológica do ser humano, tanto em seu desenvolvimento evolutivo quanto em sua expressão histórica e contemporânea, é também conhecida como Antropologia',
     'Fisica'
-}
-criarCartao{
-    'Sociologia'
-    'A recuperação da herança cultural africana deve levar em conta o que é próprio do processo cultural: seu movimento, pluralidade e complexidade. Não se trata, portanto, do resgate ingênuo do passado nem do seu cultivo nostálgico, mas de procurar perceber o próprio rosto cultural brasileiro. O que se quer é captar seu movimento para melhor compreendê-lo historicamente.'
+)
+criarCartao(
+    'Sociologia',
+    'A recuperação da herança cultural africana deve levar em conta o que é próprio do processo cultural: seu movimento, pluralidade e complexidade. Não se trata, portanto, do resgate ingênuo do passado nem do seu cultivo nostálgico, mas de procurar perceber o próprio rosto cultural brasileiro. O que se quer é captar seu movimento para melhor compreendê-lo historicamente.',
     'derivam da interação entre valores africanos e a experiência histórica brasileira.'
-}
-criarCartao{
+)
+criarCartao(
     'Sociologia'
-    'Quanto ao “choque de civilizações”, é bom lembrar a carta de uma menina americana de sete anos cujo pai era piloto na Guerra do Afeganistão: ela escreveu que – embora amasse muito seu pai – estava pronta a deixá-lo morrer, a sacrificá-lo por seu país. Quando o presidente Bush citou suas palavras, elas foram entendidas como manifestação “normal” de patriotismo americano; vamos conduzir uma experiência mental simples e imaginar uma menina árabe maometana pateticamente lendo para as câmeras as mesmas palavras a respeito do pai que lutava pelo Talibã – não é necessário pensar muito sobre qual teria sido a nossa reação.'
+    'Quanto ao “choque de civilizações”, é bom lembrar a carta de uma menina americana de sete anos cujo pai era piloto na Guerra do Afeganistão: ela escreveu que – embora amasse muito seu pai – estava pronta a deixá-lo morrer, a sacrificá-lo por seu país. Quando o presidente Bush citou suas palavras, elas foram entendidas como manifestação “normal” de patriotismo americano; vamos conduzir uma experiência mental simples e imaginar uma menina árabe maometana pateticamente lendo para as câmeras as mesmas palavras a respeito do pai que lutava pelo Talibã – não é necessário pensar muito sobre qual teria sido a nossa reação.',
     'exercício da alteridade.'
-}
+)
 
     
